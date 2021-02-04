3b1ec8ca6155c1daff7112f48d222da838b75b91
Theo Dias Mitchell <theomitchell@gmail.com>
2021-02-03 20:51:38 -0500

    Add & Configure product.component
    Show product in the Menu

---
+++ TDM_OnLineStore_Angular.Web/ClientApp/src/app/products/products.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "app-products",
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.css"]
+})
+
+export class ProductsComponent {
+  public name: string;
+  public isReadyToSale: boolean;
+
+  public getName(): string {
+    //return this.name;
+    return "Sansung";
+  }
+}
